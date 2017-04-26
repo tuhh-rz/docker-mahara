7e469bdc33362f98f11d0413028fa4f37a4872ef
Andreas Böttger <andreas.boettger@tuhh.de>
2017-04-26 11:41:48 +0200

    add new files and configurations

--- start.sh
+++ start.sh
@@ -10,6 +10,13 @@ chown -Rf www-data.www-data /var/www/html/
 #allow_call_time_pass_reference off
 sed -i 's/upload_max_filesize.*/upload_max_filesize = 100M/g' /etc/php/7.0/apache2/php.ini
 sed -i 's/post_max_size.*/post_max_size = 100M/g' /etc/php/7.0/apache2/php.ini
+sed -i 's/register_globals.*/register_globals off/g' /etc/php/7.0/apache2/php.ini
+sed -i 's/magic_quotes_runtime.*/magic_quotes_runtime off/g' /etc/php/7.0/apache2/php.ini
+sed -i 's/magic_quotes_sybase.*/magic_quotes_sybase off/g' /etc/php/7.0/apache2/php.ini
+sed -i 's/magic_quotes_gpc.*/magic_quotes_gpc = off/g' /etc/php/7.0/apache2/php.ini
+sed -i 's/log_errors.*/log_errors on/g' /etc/php/7.0/apache2/php.ini
+sed -i 's/allow_call_time_pass_reference.*/allow_call_time_pass_reference off/g' /etc/php/7.0/apache2/php.ini
+
 
 
 #sed -i 's/<\/VirtualHost>/<Location \/Shibboleth.sso>\nSetHandler shib\nAuthType None\nRequire all granted\n<\/Location>\n<Directory  \/var\/www\/html\/moodle\/auth\/shibboleth\/index.php>\nAuthType shibboleth\nShibRequireSession On\nrequire valid-user\n<\/Directory>\n<\/VirtualHost>/' /etc/apache2/sites-available/default-ssl.conf
@@ -19,4 +26,4 @@ sed -i 's/<\/VirtualHost>/<Directory \/var\/www\/html>\nAllowOverride ALL\n<\/Di
 rsync -rc /tmp/mahara/* "/var/www/html"
 chown -Rf www-data.www-data "/var/www/html"
 
-/usr/local/bin/supervisord -n -c /etc/supervisord.conf 
+/usr/local/bin/supervisord -n -c /etc/supervisord.conf
